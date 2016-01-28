5b48548b48010e129b75aaac6d5ceda6353afdba
Henry Partridge <hcpartridge@HENRYs-MacBook-Pro.local>
2016-01-28 09:44:22 +0000

    Initial commit

---
+++ crime_map/about.md
@@ -0,0 +1,29 @@
+---
+  title: "about"
+output:
+  html_document:
+  highlight: monochrome
+theme: spacelab
+---
+
+<br>
+
+#### Overview
+This [shiny](http://shiny.rstudio.com") app enables users to interrogate open source police crime data recorded by Greater Manchester Police. 
+
+#### Data sources
+Greater Manchester Police recorded crime are available from [data.police.uk](https://data.police.uk/). The data are subject to an anonymisation process which means that crimes are geocoded to approximate not actual locations. For more information visit: [https://data.police.uk/about/#anonymisation](https://data.police.uk/about/#anonymisation)
+
+#### Credits
+The [leaflet](https://rstudio.github.io/leaflet/) and [dygraphs](https://rstudio.github.io/dygraphs/) R packages were used in this [shiny](http://shiny.rstudio.com") app.
+
+#### Licence
+The vector boundaries for Greater Manchester's metropolitan districts were published by the Office for National Statistics and are available from [data.gov.uk](https://data.gov.uk/dataset/county-and-unitary-authorities-ew-2012-boundaries-full-extent). This information is licensed under the terms of the [Open Government Licence](http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3).
+
+#### Code                          
+The GitHub respository containing the R code, crime data and vector boundaries can be found [here](https://github.com/cat-lord/shinyapps/tree/master/crime_map).
+
+
+
+
+
---
+++ crime_map/app.R
@@ -0,0 +1,149 @@
+library(shiny) ; library(sp); library(rgdal) ; library(leaflet) ; library(dygraphs) ; library(xts) ; library(dplyr) ; library(markdown)
+
+crimes <- read.csv("crime_data.csv", header = T)
+boroughs <-  readOGR("boroughs.geojson", "OGRGeoJSON")
+
+ui <- navbarPage(title = "Crime map",
+                 tabPanel("Interactive plots",
+                          column(3,
+                                 div(class="outer",
+                                     tags$head(includeCSS('custom.css')),
+                                     absolutePanel(id = "controls", class="panel panel-default", draggable = FALSE, fixed = TRUE, 
+                                                   top = 85, left = 80, right = "auto", bottom = "auto", height = "auto", width = 260,
+                                          h3("Instructions"),
+                                          p('This Shiny app allows you to interactively visualise Greater Manchester Police 
+                                            crime data downloaded from', a("data.police.uk", href = "https://data.police.uk")),
+                                          p('Use the dropdown menus below to select the borough and crime category 
+                                                   of interest.'), 
+                                          p('Then zoom and pan around the map to explore clusters of crime. Click on
+                                                   the red circles for information on individual crimes.'), 
+                                          p('The map will also update when you interact with the time series chart.'),
+                                          br(),
+                                          br(),
+                                          br(),
+                                          div(uiOutput('borough'), style = "color:#525252", align = "left"),
+                                          div(uiOutput('category'), style = "color:#525252", align = "left")))),
+                            column(8,
+                                   br(),
+                                   div(h4(textOutput("title"), align = "left"), style = "color:#525252"),
+                                   div(h5(textOutput("period"), align = "left"), style = "color:#525252"),
+                                   fluidRow(
+                                    leafletOutput("map", width = "100%", height = "350"),
+                                            absolutePanel(id = "controls", class="panel panel-default", draggable = TRUE, fixed = TRUE,
+                                                 top = 190, left = "auto", right = 160, bottom = "auto", height = "20", width = "200",
+                                                 strong(textOutput("frequency"), style = "color:red", align = "left"))),
+                                   fluidRow(
+                                     br(),
+                                     dygraphOutput("dygraph", width = "100%", height = "100px")))),
+                 tabPanel("About",
+                          fluidRow(
+                            column(8, offset = 1,
+                                   includeMarkdown("about.md")))))
+
+server <- function(input, output, session) {
+  
+  output$borough <- renderUI({
+    selectInput("borough", label = "Select a borough",
+                choices = levels(droplevels(crimes$borough)),
+                selected = "Manchester")
+  })
+  
+  output$category <- renderUI({
+    selectInput("category", label = "Select a crime category",
+                  choices = levels(droplevels(crimes$category)),
+                selected = "Burglary")
+  })
+    
+  selected_crimes <- reactive({crimes %>% 
+      filter(borough == input$borough & category == input$category)})
+  
+  output$title <- renderText({
+    paste0(input$category, " in ", input$borough)
+  })
+  
+  output$period <- renderText({
+    req(input$dygraph_date_window[[1]])
+    paste0("(", strftime(input$dygraph_date_window[[1]], "%B %Y"), " - ", 
+           strftime(input$dygraph_date_window[[2]], "%B %Y"), ")")
+  })  
+  
+  output$dygraph <- renderDygraph({
+    req(input$category)
+      df <- selected_crimes() %>% 
+        mutate(month = as.Date(month, format = '%Y-%m-%d')) %>%
+        group_by(month) %>%
+        summarize(n = n()) %>%
+        select(month, n)
+      
+      df.xts <- xts(df$n, order.by = as.Date(df$month, "%Y-%m-%d"), frequency = 12)
+      
+      dygraph(df.xts, main = NULL) %>%
+        dySeries("V1", label = "Crimes", color = "red", fillGraph = TRUE, strokeWidth = 2, drawPoints = TRUE, pointSize = 4) %>%
+        dyAxis("y", axisLabelWidth = 20) %>% 
+        dyOptions(retainDateWindow = TRUE, includeZero = TRUE, drawGrid = FALSE,
+                  axisLineWidth = 2, axisLineColor = "#525252", axisLabelFontSize = 11, axisLabelColor = "#525252") %>% 
+        dyLegend(width = 200) %>% 
+        dyCSS("dygraph.css")
+  })
+  
+  points <- reactive({crimes %>% 
+      mutate(month = as.Date(month, format = '%Y-%m-%d')) %>%
+      filter(borough == input$borough & 
+               category == input$category &
+               month >= input$dygraph_date_window[[1]], month <= input$dygraph_date_window[[2]])
+    
+  })
+  
+  output$map <- renderLeaflet({
+    
+    req(input$dygraph_date_window[[1]])
+    
+    popup <- paste0("<strong>Location: </strong>", points()$location,
+                    "<br><strong>Borough: </strong>", points()$borough,
+                    "<br><strong>Category: </strong>", points()$category,
+                    "<br><strong>Date: </strong>", points()$month)
+    
+      leaflet(points()) %>%
+      addProviderTiles("CartoDB.Positron") %>% 
+      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat)) %>% 
+      addPolygons(data = boroughs, color = "#525252", weight = 2, fillColor = "transparent") %>% 
+      addCircleMarkers(data = points(), ~long, ~lat, radius = 5, stroke = TRUE,
+                       color = "red", weight = 3, opacity = 0.8, fillColor = "white",
+                       popup = popup,
+                       clusterOptions = markerClusterOptions(
+                         # zoom to cluster bounds when clicked
+                         zoomToBoundsOnClick = TRUE,
+                         # render cluster markers when lowest zoom level clicked
+                         spiderfyOnMaxZoom = TRUE, 
+                         # maximum cluster radius in pixels from central marker
+                         maxClusterRadius = 50))
+
+  })
+ 
+  dataInBounds <- reactive({
+    df <- points()
+    if (is.null(input$map_bounds))
+      return(df[FALSE,])
+    bounds <- input$map_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    subset(df,
+           lat >= latRng[1] & lat <= latRng[2] &
+             long >= lngRng[1] & long <= lngRng[2])
+  })
+  
+  output$frequency <- renderText({
+    req(input$map_bounds)
+    
+    df <- dataInBounds() %>% 
+      group_by(category) %>%
+      summarize(n = n())
+    
+    paste0(df$n, " crimes displayed")
+  })
+  
+  
+}
+
+shinyApp(ui, server)
